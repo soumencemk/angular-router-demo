a191c0fb816675a6ebd736cf30b50f4a4f0981db
Soumen Karmakar <soumen.karmakar@ericsson.com>
2024-10-14 13:34:02 +0100

    Adding material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ContactComponent } from './contact/contact.component';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProductsModule } from './products/products.module';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { PostsModule } from './posts/posts.module';
 import { LoginComponent } from './login/login.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponent,
-    NotFoundComponent, 
-    ContactComponent, LoginComponent,   
+    NotFoundComponent,
+    ContactComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
@@ -30,8 +38,16 @@ import { LoginComponent } from './login/login.component';
     ProductsModule,
     HttpClientModule,
     PostsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatDividerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { ProductdetailsComponent } from './productdetails/productdetails.component';
 import { AppRoutingModule } from '../app-routing.module';
-
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    ProductlistComponent,
-    ProductdetailsComponent
-  ],
-  imports: [    
-    CommonModule,
-    AppRoutingModule
-  ]
+  declarations: [ProductlistComponent, ProductdetailsComponent],
+  imports: [CommonModule, AppRoutingModule, MatDividerModule, MatListModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
